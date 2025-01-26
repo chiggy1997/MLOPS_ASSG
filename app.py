1ca84b54e60549b288197c08f8a49daafdabb2de
Chirag Chopra <35568354+chiggy1997@users.noreply.github.com>
2025-01-26 13:32:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 app = Flask(__name__)
 
 # Load the trained model
-with open('model.pkl', 'rb') as f:
+with open('svm.pkl', 'rb') as f:
     model = pickle.load(f)
 
 @app.route('/')
